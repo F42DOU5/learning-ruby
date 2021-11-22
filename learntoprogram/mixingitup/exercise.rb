d8795bf2eec031bdc9e8d0952d6d3fe30555060f
fardousks97 <iamhasanalfardous@gmail.com>
2021-11-23 00:35:41 +0600

    Revert "feat;learn to program chapter-five"
    This reverts commit 38a2b82d7e564bf20a55e9fb7824ac254991c12a.

--- learntoprogram/mixingitup/exercise.rb
+++
@@ -1,7 +0,0 @@
-puts 'what is your first name'
-fname = gets.chomp
-puts 'then what is your middle name'
-mname = gets.chomp
-puts 'and what is your last name'
-lname = gets.chomp
-puts 'Nice to meet you' + fname + mname + lname + ':)'
